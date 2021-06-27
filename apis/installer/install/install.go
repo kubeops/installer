d4eda51e33280cb9861c18317ebd42e4a394bab6
Tamal Saha <tamal@appscode.com>
2021-06-26 18:23:30 -0700

    Add cleaner and registryFQDN support (#3)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/install/install.go
+++ apis/installer/install/install.go
@@ -17,7 +17,7 @@ limitations under the License.
 package install
 
 import (
-	"kubeops.dev/panopticon-installer/apis/installer/v1alpha1"
+	"kubeops.dev/installer/apis/installer/v1alpha1"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
--- apis/installer/v1alpha1/doc.go
+++ apis/installer/v1alpha1/doc.go
@@ -17,7 +17,7 @@ limitations under the License.
 // Package v1alpha1 is the v1alpha1 version of the API.
 
 // +k8s:deepcopy-gen=package,register
-// +k8s:conversion-gen=kubeops.dev/panopticon-installer/apis/installer
+// +k8s:conversion-gen=kubeops.dev/installer/apis/installer
 // +k8s:openapi-gen=true
 // +k8s:defaulter-gen=TypeMeta
 
