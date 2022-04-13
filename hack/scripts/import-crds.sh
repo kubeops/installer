4e124eb44a8b5cb255c7c409bf871172a22facc1
Tamal Saha <tamal@appscode.com>
2022-04-13 15:29:13 -0700

    Update crds
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/import-crds.sh
+++ hack/scripts/import-crds.sh
@@ -27,4 +27,37 @@ crd-importer \
     --input=https://github.com/prometheus-operator/prometheus-operator/raw/v0.54.1/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml \
     --out=./charts/panopticon/crds
 
-# Todo(Pulak): Import CRDs
+{
+    supervisor_dir=${1:-}/supervisor/crds
+
+    supervisor_repo_url=https://github.com/kubeops/supervisor.git
+    supervisor_repo_tag=${KUBEOPS_SUPERVISOR_TAG:-master}
+
+    if [ "$#" -ne 1 ]; then
+        if [ "${supervisor_repo_tag}" == "master" ]; then
+            echo "Error: missing path_to_input_crds_directory"
+            echo "Usage: import-crds.sh <path_to_input_crds_directory>"
+            exit 1
+        fi
+
+        tmp_dir=$(mktemp -d -t api-XXXXXXXXXX)
+        # always cleanup temp dir
+        # ref: https://opensource.com/article/20/6/bash-trap
+        trap \
+            "{ rm -rf "${tmp_dir}"; }" \
+            SIGINT SIGTERM ERR EXIT
+
+        mkdir -p ${tmp_dir}
+        pushd $tmp_dir
+        git clone $supervisor_repo_url
+        repo_dir=$(ls -b1)
+        cd $repo_dir
+        git checkout $supervisor_repo_tag
+        popd
+        supervisor_dir=${tmp_dir}/${repo_dir}/crds
+    fi
+
+    crd-importer \
+        --input=${supervisor_dir} \
+        --out=./charts/supervisor/crds
+}
