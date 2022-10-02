91da9d58fcef02a8b3c3f0f4eb08482be615d7d8
Tamal Saha <tamal@appscode.com>
2022-10-02 13:35:38 -0700

    Add scanner installer (#53)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/register.go
+++ apis/installer/v1alpha1/register.go
@@ -62,6 +62,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&KubeUiServerList{},
 		&Panopticon{},
 		&PanopticonList{},
+		&Scanner{},
+		&ScannerList{},
 		&Supervisor{},
 		&SupervisorList{},
 	)
