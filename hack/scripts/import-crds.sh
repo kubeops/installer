47c28467a6a3bbf505f9a3e99e57f359ea09ce17
Tamal Saha <tamal@appscode.com>
2023-09-23 18:15:46 -0700

    Add ProjectQuota crd
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/import-crds.sh
+++ hack/scripts/import-crds.sh
@@ -34,6 +34,7 @@ crd-importer \
     --input=https://github.com/fluxcd/helm-controller/raw/v0.28.1/config/crd/bases/helm.toolkit.fluxcd.io_helmreleases.yaml \
     --input=https://github.com/fluxcd/source-controller/raw/v0.30.1/config/crd/bases/source.toolkit.fluxcd.io_helmrepositories.yaml \
     --input=https://github.com/kmodules/custom-resources/raw/v0.25.1/crds/appcatalog.appscode.com_appbindings.yaml \
+    --input=https://github.com/kmodules/resource-metadata/raw/master/crds/management.k8s.appscode.com_projectquotas.yaml \
     --input=https://github.com/kmodules/resource-metadata/raw/master/crds/ui.k8s.appscode.com_features.yaml \
     --input=https://github.com/kmodules/resource-metadata/raw/master/crds/ui.k8s.appscode.com_featuresets.yaml \
     --input=https://github.com/kmodules/resource-metadata/raw/master/crds/ui.k8s.appscode.com_resourcedashboards.yaml \
