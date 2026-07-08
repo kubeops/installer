b626096c74aa3738c2b24f51597327bab5229630
Tamal Saha <tamal@appscode.com>
2026-07-08 13:57:26 +0600

    Set config-syncer CI license via LICENSE_KEY in unit-tests (#498)
    Inject the config-syncer license into charts/config-syncer/ci/ci-values.yaml
    from a LICENSE_KEY env var in Go TestMain (using yaml.v3) instead of a yq
    step, and pass LICENSE_KEY through the unit-tests make target. Drop the
    now-unneeded yq/kubectl host-prep step from the CI build job.
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- tests/check-charts_test.go
+++ tests/check-charts_test.go
@@ -18,12 +18,41 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"path/filepath"
 	"runtime"
 	"testing"
 
 	"kmodules.xyz/image-packer/pkg/lib"
+	"sigs.k8s.io/yaml"
 )
+
+// TestMain sets the license in the config-syncer CI values before running the
+// tests
+func TestMain(m *testing.M) {
+	if err := setConfigSyncerLicense(os.Getenv("TEST_LICENSE_KEY")); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	os.Exit(m.Run())
+}
+
+func setConfigSyncerLicense(licenseKey string) error {
+	dir, err := rootDir()
+	if err != nil {
+		return err
+	}
+	path := filepath.Join(dir, "charts", "config-syncer", "ci", "ci-values.yaml")
+
+	out, err := yaml.Marshal(map[string]any{
+		"license": licenseKey,
+	})
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, out, 0o644)
+}
 
 func Test_CheckImageArchitectures(t *testing.T) {
 	dir, err := rootDir()
